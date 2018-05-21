61f8c0a78c8a9724459971fa63e8ecbd9f1ed149
Nicholas Jonker <njonker@entersekt.com>
2018-05-21 18:50:02 +0200

    added vuex

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,26 +1,26 @@
 <template>
   <v-container>
-    <v-layout row wrap>
+    <v-layout row wrap v-for="meetup in meetups" :key="meetup.id" class="mb-2">
       <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
         <v-card class="info">
           <v-container fluid>
             <v-layout row>
               <v-flex xs5 sm4 md3>
                 <v-card-media
-                  src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
+                  :src="meetup.imageUrl"
                   height="130px">
                 </v-card-media>
               </v-flex>
               <v-flex xs7 sm8 md9>
                 <v-card-title primary-title>
                   <div>
-                    <h3 class="white--text mb-0">My Meetup</h3>
-                    <div>17 July 2018</div>
+                    <h3 class="white--text mb-0">{{ meetup.title}}</h3>
+                    <div>{{ meetup.date }}</div>
                   </div>
                 </v-card-title>
                 <v-card-actions>
-                  <v-btn flat to="/meetups/1">
-                    <v-icon light>arrow_forward</v-icon>
+                  <v-btn flat :to="'/meetups/' + meetup.id">
+                    <v-icon left light>arrow_forward</v-icon>
                     View Meetup
                   </v-btn>
                 </v-card-actions>
@@ -32,3 +32,13 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    computed: {
+      meetups () {
+        return this.$store.getters.loadedMeetups
+      }
+    }
+  }
+</script>
